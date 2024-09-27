d83a80c0c3101630b56a268f68d20ec6f849925e
youaodu <49941556+youaodu@users.noreply.github.com>
2024-09-27 14:31:59 +0800

    增加注释

--- P1216数字三角形.cpp
+++ P1216数字三角形.cpp
@@ -5,15 +5,23 @@ int r;
 int table[1005][1005];
 long long result[1005][1005];
 
+// 递归方法
 int computeMax(int i, int j) {
+	// 已经算过的节点就不需要再算一次了。
 	if (result[i][j] > -1) {
 		return result[i][j];
 	}
+
+	// 如果+1大于r的话，代表着已经是最后一行或者最后一列了。
 	if (i + 1 > r || j + 1 > r) {
 		return table[i][j];
 	}
+
+	// 计算左边的值
 	int left = computeMax(i + 1, j);
+	// 计算右边的值
 	int right = computeMax(i + 1, j+1);
+	// 将计算过的值缓存下来
 	result[i][j] = max(left, right) + table[i][j];
 	return result[i][j];
 }
